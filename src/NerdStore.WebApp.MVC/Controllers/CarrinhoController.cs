91df7a3945a9da3ca17cca8401e0738612b97bed
Bryan <bryanlima200@outlook.com>
2022-08-07 13:10:46 -0300

    Configured launch of notifications

--- src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
+++ src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using NerdStore.Catalogo.Application.Services;
 using NerdStore.Core.Communication.Mediator;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
 using NerdStore.Vendas.Application.Commands;
 
 namespace NerdStore.WebApp.MVC.Controllers
@@ -10,8 +12,9 @@ namespace NerdStore.WebApp.MVC.Controllers
         private readonly IProdutoAppService _produtoAppService;
         private readonly IMediatorHandler _mediatorHandler;
 
-        public CarrinhoController(IProdutoAppService produtoAppService, 
-                                  IMediatorHandler mediatorHandler)
+        public CarrinhoController(INotificationHandler<DomainNotification> notifications,
+                                  IProdutoAppService produtoAppService, 
+                                  IMediatorHandler mediatorHandler) : base(notifications, mediatorHandler)
         {
             _produtoAppService = produtoAppService;
             _mediatorHandler = mediatorHandler;
@@ -40,7 +43,10 @@ namespace NerdStore.WebApp.MVC.Controllers
             await _mediatorHandler.EnviarComando(command);
 
 
-            // TODO: se deu certo...
+            if (OperacaoValida())
+            {
+                return RedirectToAction("Index");
+            }
 
             TempData["Erro"] = "Produto indisponível";
             return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
