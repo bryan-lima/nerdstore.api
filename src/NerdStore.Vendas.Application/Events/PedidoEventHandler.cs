44080c6a672aadc8a2a060a5d8fe7625e5c9d331
Bryan <bryanlima200@outlook.com>
2022-08-07 19:53:12 -0300

    Implemented stock validation to proceed (for payment) or cancel the order

--- src/NerdStore.Vendas.Application/Events/PedidoEventHandler.cs
+++ src/NerdStore.Vendas.Application/Events/PedidoEventHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using NerdStore.Core.Messages.CommonMessages.IntegrationEvents;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,8 @@ namespace NerdStore.Vendas.Application.Events
     public class PedidoEventHandler :
         INotificationHandler<PedidoRascunhoIniciadoEvent>,
         INotificationHandler<PedidoAtualizadoEvent>,
-        INotificationHandler<PedidoItemAdicionadoEvent>
+        INotificationHandler<PedidoItemAdicionadoEvent>,
+        INotificationHandler<PedidoEstoqueRejeitadoEvent>
     {
         public Task Handle(PedidoRascunhoIniciadoEvent notification, CancellationToken cancellationToken)
         {
@@ -24,6 +26,13 @@ namespace NerdStore.Vendas.Application.Events
 
         public Task Handle(PedidoItemAdicionadoEvent notification, CancellationToken cancellationToken)
         {
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(PedidoEstoqueRejeitadoEvent notification, CancellationToken cancellationToken)
+        {
+            // Cancelar o processamento do pedido - Retornar erro para o cliente
+
             return Task.CompletedTask;
         }
     }
