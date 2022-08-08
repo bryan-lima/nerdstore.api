a77748c74a28b0478811e73e38fb6801b53e6f91
Bryan <bryanlima200@outlook.com>
2022-08-07 21:29:39 -0300

    Implemented bounded context Payments

---
+++ src/NerdStore.Pagamentos.Business/IPagamentoService.cs
@@ -0,0 +1,14 @@
+﻿using NerdStore.Core.DomainObjects.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Pagamentos.Business
+{
+    public interface IPagamentoService
+    {
+        Task<Transacao> RealizarPagamentoPedido(PagamentoPedido pagamentoPedido);
+    }
+}
